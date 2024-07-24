f8dbf6c8faa7a48db8ba7d8f4a754f0bede90cd8
Divyarajsinh01 <divyarajsinhrayjada3@gmail.com>
2024-07-24 11:37:49 +0530

    changes

--- controller/chapter.controller.js
+++ controller/chapter.controller.js
@@ -2,16 +2,18 @@ const Chapter = require("../model/chapter.model")
 
 exports.addChapter = async (req, res) => {
     try {
-        const {chapter_number, chapter, teacher_name,total_questions,minutes,sub_id} = req.body
+        const { chapter, teacher_name,total_questions,minutes,sub_id} = req.body
 
+        //find last chapter based on chapter number with subject
         const chapter_index = await Chapter.findOne({sub_id}).sort({chapter_number: -1})
 
-        console.log(chapter_index);
+        // console.log(chapter_index);
 
+        //auto-increment chapter number based on last chapter number store in database 
         const increment_chapter_number = chapter_index ? chapter_index.chapter_number + 1 : 1
         // console.log(increment_chapter_number);
 
-        const chpaterData = new Chapter({
+        const chapterData = new Chapter({
             chapter_number: increment_chapter_number,
             chapter,
             teacher_name,
@@ -20,11 +22,34 @@ exports.addChapter = async (req, res) => {
             sub_id
         })
 
-        await chpaterData.save()
+        await chapterData.save()
 
         res.status(200).json({
             message: 'successfully chapter added!',
-            data: chpaterData
+            data: chapterData
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+    }
+}
+
+
+exports.getChapters = async (req, res) => {
+    try {
+        const chapter = await Chapter.find().populate({
+            path: 'sub_id',
+            modal: 'subject',
+            populate: {
+              path: 'std_id',
+              model: 'Standard'
+            }
+          })
+
+        res.status(400).json({
+            message: 'chapter fetch successfully!',
+            data: chapter
         })
     } catch (error) {
         res.status(400).json({
--- router/chapter.router.js
+++ router/chapter.router.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const { addChapter } = require('../controller/chapter.controller')
+const { addChapter, getChapters } = require('../controller/chapter.controller')
 
 const chapterRouter = express.Router()
 
 chapterRouter.post('/add/chapter', addChapter)
-
+chapterRouter.get('/get/chapter', getChapters)
 
 module.exports = chapterRouter
