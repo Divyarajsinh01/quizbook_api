15954dd7c4a31b66e7e6df1cdfddcd96ca21e969
Kartikpatel0910 <kartikviradiya10@gmail.com>
2024-07-24 11:06:48 +0530

    done

--- controller/chapter.controller.js
+++ controller/chapter.controller.js
@@ -4,8 +4,15 @@ exports.addChapter = async (req, res) => {
     try {
         const {chapter_number, chapter, teacher_name,total_questions,minutes,sub_id} = req.body
 
+        const chapter_index = await Chapter.findOne({sub_id}).sort({chapter_number: -1})
+
+        console.log(chapter_index);
+
+        const increment_chapter_number = chapter_index ? chapter_index.chapter_number + 1 : 1
+        // console.log(increment_chapter_number);
+
         const chpaterData = new Chapter({
-            chapter_number,
+            chapter_number: increment_chapter_number,
             chapter,
             teacher_name,
             total_questions,
