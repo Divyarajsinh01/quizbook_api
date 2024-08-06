4d6041c47fcc7aa3a7b4927ffe964543bd284f91
Kartikpatel0910 <kartikviradiya10@gmail.com>
2024-08-06 11:00:47 +0530

    changes

--- controller/result.controller.js
+++ controller/result.controller.js
@@ -4,8 +4,8 @@ const Subject = require("../model/subject.model")
 
 exports.addResults = async (req, res) => {
     try {
-        const { std_id, sub_id, chapter_id, questions } = req.body
-        const user_id = req.user._id
+        const { std_id, sub_id, chapter_id, questions } = req.body;
+        const user_id = req.user._id;
 
         const result = new Result({
             user_id,
@@ -13,11 +13,9 @@ exports.addResults = async (req, res) => {
             sub_id,
             chapter_id,
             questions
-        })
+        });
 
-        await result.save()
-
-        // const resultData = await Result.findOne({user_id}).populate('std_id')
+        await result.save();
 
         const resultData = await Result.aggregate([
             { $match: { _id: result._id } },
@@ -29,7 +27,7 @@ exports.addResults = async (req, res) => {
                     as: 'standard'
                 }
             },
-            // {$unwind: '$standard'},
+            { $unwind: '$standard' },
             {
                 $lookup: {
                     from: 'subjects',
@@ -38,7 +36,7 @@ exports.addResults = async (req, res) => {
                     as: 'subject'
                 }
             },
-            // {$unwind: '$subject'},
+            { $unwind: '$subject' },
             {
                 $lookup: {
                     from: 'chapters',
@@ -47,26 +45,79 @@ exports.addResults = async (req, res) => {
                     as: 'chapter'
                 }
             },
-            // {$unwind: '$chapter'},
+            { $unwind: '$chapter' },
             {
                 $lookup: {
                     from: 'questions',
                     localField: 'questions.question_id',
                     foreignField: '_id',
-                    as: 'questiondeatils'
+                    as: 'questionDetails'
                 }
             },
-            // {$unwind: '$questiondeatils'},
-        ])
+            {
+                $project: {
+                    std_id: 1,
+                    standard: '$standard.standard',
+                    sub_id: 1,
+                    subject: '$subject.subject',
+                    chapter_id: 1,
+                    chapter_name: '$chapter.chapter_name',
+                    chapter_number: '$chapter.chapter_number',
+                    teacher_name: '$chapter.teacher_name',
+                    questions: {
+                        $map: {
+                            input: '$questions',
+                            as: 'q',
+                            in: {
+                                question_id: '$$q.question_id',
+                                user_answer: '$$q.user_answer',
+                                question_detail: {
+                                    $arrayElemAt: [
+                                        '$questionDetails',
+                                        { $indexOfArray: ['$questionDetails._id', '$$q.question_id'] }
+                                    ]
+                                }
+                            }
+                        }
+                    }
+                }
+            }, {
+                $project: {
+                    std_id: 1,
+                    standard: 1,
+                    sub_id: 1,
+                    subject: 1,
+                    chapter_id: 1,
+                    chapter_name: 1,
+                    chapter_number: 1,
+                    teacher_name: 1,
+                    questions: {
+                        $map: {
+                            input: '$questions',
+                            as: 'q',
+                            in: {
+                                question_id: '$$q.question_id',
+                                user_answer: '$$q.user_answer',
+                                question_number: '$$q.question_detail.question_number',
+                                question_name: '$$q.question_detail.question',
+                                option: '$$q.question_detail.option',
+                                right_answer: '$$q.question_detail.right_answer'
+                            }
+                        }
+                    }
+                }
+            }
+
+        ]);
 
         res.status(200).json({
-            message: 'result added successfully!',
+            message: 'Result added successfully!',
             data: resultData
-        })
+        });
 
     } catch (error) {
         res.status(400).json({
             message: error.message
-        })
+        });
     }
-}
+};
